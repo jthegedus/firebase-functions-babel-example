e00061ddcd7c184857dcd7a285b2daed5d1a168e
James <jthegedus@jttech.io>
2017-11-29 23:58:42 +1100

    Add async await to example

--- firebaseFunctions/index.js
+++ firebaseFunctions/index.js
@@ -1,6 +1,14 @@
 import * as functions from "firebase-functions"
 
-export let helloWorld = functions.https.onRequest((req, res) => {
-  let world = `from preset-env Babelified Cloud Functions!`
+const message = () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve(`from Babelified Cloud Functions!`)
+    }, 5000)
+  })
+}
+
+export let helloWorld = functions.https.onRequest(async (req, res) => {
+  let world = await message()
   res.status(200).send(`Hello ${world}`)
 })
